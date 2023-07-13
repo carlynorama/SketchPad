874816c8c4123064dcb0217e6e10c8289b4b06c6
carlynorama <carlynorama@gmail.com>
2023-07-13 07:31:21 -0700

    toss in the x3d

---
+++ Sources/SketchPad/FileBuilders/X3DFileBuilder.swift
@@ -0,0 +1,107 @@
+//
+//  X3DFileBuilder.swift
+//
+//
+//  Created by Carlyn Maw on 7/12/23.
+//
+
+import Foundation
+
+public struct X3DFileBuilder {
+    // let title:String
+    // let description:String
+
+    public init() { }
+
+    //TODO: fix String builder so if's aren't empty lines
+    @StringBuilder func generateHeader(
+        title:String? = nil, 
+        description:String? = nil
+        ) -> String {
+        #"<?xml version="1.0" encoding="UTF-8"?>"#
+        #"<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 3.1//EN" "http://www.web3d.org/specifications/x3d-3.1.dtd">"#
+        #"<X3D profile="Immersive" version="3.1" xsd:noNamespaceSchemaLocation="http://www.web3d.org/specifications/x3d-3.1.xsd" xmlns:xsd="http://www.w3.org/2001/XMLSchema-instance">"#
+        #"<head>"#
+        if let title { "\t<meta content=\"\(title)\" name=\"title\"/>"}
+        if let description {"\t<meta content=\"\(description)\" name=\"description\"/>"} 
+        "\t\(dateMetaString())"
+        "\t<meta content=\"SketchPad\" name=\"generator\"/>"
+        "</head>"
+    }
+
+    //TODO: Date actually generated
+    func dateMetaString() -> String {
+        "<meta content=\"13 Jul 2023\" name=\"created\"/>"
+    }
+
+    @StringBuilder func sceneHeader() -> String {
+        "<Scene>"
+    }
+
+    @StringBuilder func sceneFooter() -> String {
+         "</Scene>"
+    }
+
+    @StringBuilder func pageFooter() -> String {
+        "</X3D>"
+    }
+
+    func transformStart(transform:Transformation) -> String {
+        switch transform {
+        case .translate(let v):
+        return "<transform translation='\(v.x) \(v.y) \(v.z)'>" 
+        // default:
+        // fatalError()
+        }
+    }
+
+    func transformClose() -> String {
+        "</transform>"
+    }
+
+    func materialString(_ surfaces:[Surface]) -> String {
+        //<material DEF="color" diffuseColor=" 0.6 0 0" specularColor='1 1 1'/>
+        //<material diffuseColor='0 0 1'></material> 
+        if surfaces.count > 1 { fatalError() }
+        let surface = surfaces[0]
+        switch surface {
+            case .displayColor(let c):
+                return "<material diffuseColor='\(c.r) \(c.g) \(c.b)'></material>"
+            default:
+                fatalError()
+        }
+    }
+
+     @StringBuilder func sphereBuilder(shape:Sphere) -> String {
+        if !shape.transformations.isEmpty {
+            for transform in shape.transformations {
+                transformStart(transform:transform)
+            }
+        }
+        "<shape>"
+        "\t<appearance>" 
+        if !shape.surfaces.isEmpty {
+            materialString(shape.surfaces)
+        }
+        "\t</appearance>" 
+        //TODO: what does a unit mean here vs. usd?
+        "\t<sphere radius='\(shape.radius)'></sphere>" 
+        "</shape>"
+        if !shape.transformations.isEmpty {
+            for _ in 0..<shape.transformations.count {
+                transformClose()
+            }
+        }
+     }
+
+    @StringBuilder public func generateStringForStage(stage:Canvas3D) -> String {
+        generateHeader()
+        sceneHeader()
+        for item in stage.content {
+            sphereBuilder(shape: item)
+        }
+        sceneFooter()
+        pageFooter()
+    }
+}
+
--- Sources/SketchPadCLI/multiball.swift
+++ Sources/SketchPadCLI/multiball.swift
@@ -27,23 +27,43 @@ extension SketchPadCLI {
         CommandConfiguration(abstract: "Generate a USDA file that references sphere_base.usda like previous examples. 12 + blue origin ball is the default count")
         
         func run() {
-            let fileBuilder = USDAFileBuilder()
-            let fileString:String = fileBuilder.generateStringForStage(stage: MultiBallStage(count:count).buildStage())
+            let stage = MultiBallStage(count:count).buildStage()
+            let fileBuilder_x3d = X3DFileBuilder()//USDAFileBuilder()
+            let fileString_x3d:String = fileBuilder_x3d.generateStringForStage(stage: stage)
+            let fileBuilder_usd = USDAFileBuilder()//USDAFileBuilder()
+            let fileString_usd:String = fileBuilder_usd.generateStringForStage(stage: stage)
+
             if saveToFile || customPath != nil {
-                do {
-                    guard let data:Data = fileString.data(using: .utf8) else {
-                        print("Could not encode string to data")
-                        return
-                    }
-                let path:String = customPath ?? "multiball_\(FileIO.timeStamp()).usda"
-                    try FileIO.writeToFile(data: data, filePath: path)
-                } catch {
-                    print("Could not write data to file: \(error)")
-                }
+                let timeStamp = FileIO.timeStamp()
+                let path = customPath ?? "multiball_\(timeStamp).usda"
+                saveFile(fileString: fileString_usd, path: path)
+                let path_x3d = "multiball_\(timeStamp).x3d"
+                saveFile(fileString: fileString_x3d, path: path_x3d)
             } else {
-                print(fileString)
+                print(fileString_usd)
+                print("------------------")
+                print(fileString_x3d)
             }
         }
         
+        func saveFile(fileString:String, path:String, ext:String? = nil) {
+            do {
+                guard let data:Data = fileString.data(using: .utf8) else {
+                    print("Could not encode string to data")
+                    return
+                }
+                var fullPath:String
+                if let ext {
+                    fullPath = "\(path).\(ext)"
+                } else {
+                    fullPath = path
+                }
+                try FileIO.writeToFile(data: data, filePath: fullPath)
+            } catch {
+                print("Could not write data to file: \(error)")
+            }
+        }
     }
+
+    
 }
