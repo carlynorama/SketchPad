4855d935d9e4061006913c5c7e6c7294f299634e
carlynorama <carlynorama@gmail.com>
2023-07-17 20:03:23 -0700

    18 default count

--- Sources/SketchPadCLI/multiball.swift
+++ Sources/SketchPadCLI/multiball.swift
@@ -20,7 +20,7 @@ extension SketchPadCLI {
         
         @Option(name: [.customLong("count"), .customShort("c")],
                 help: "Number of spheres to generate in addition to the blue origin sphere. Default is 12")
-        var count:Int = 2
+        var count:Int = 18
         
         
         static var configuration =
